003237e20f0bff8e47eca04bfc67e6ae48893570
adarsul <62387353+adarsul@users.noreply.github.com>
2021-08-01 18:27:11 +0300

    Update tareIt_Function.ino

--- Scale_Irrigation/Calibration/tareIt_Function.ino
+++ Scale_Irrigation/Calibration/tareIt_Function.ino
@@ -1,19 +1,20 @@
+//This function returns the intercept value using an average value from a time frame
+float slope = -0.0098;
+
 float tareIt (int sec){
   Serial.println("Tare start...");
   float total, zero, average;
-  int reps = sec * 10;
+  int reps = sec * 10;  //10 readings per second
   delay(1000);
   for (int i = 0; i < reps ; i++){
     if (scale.is_ready()) {
     long reading = scale.read();
     zero = 0 - (slope*(float)reading);
     total = total + zero;
-    Serial.println("good");
     delay(100);
     }
-    else{
+    else{   //If no reading then repeat loop
       i--;
-      Serial.println("bad");
     }
   }
   average = total / reps;
